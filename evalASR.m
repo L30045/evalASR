6ff5afed0f891ce0f9699bd1d6da5db2c084125c
Chi Yuan Chang <yuan@seeing.ucsd.edu>
2017-12-20 15:01:15 -0800

    [evalASR.m] Bad Ch rm [related_paper] PREP

--- evalASR.m
+++ evalASR.m
@@ -6,3 +6,36 @@ if isempty(eeglab_path)
     cd(current_path);
     addpath(genpath('./'));
 end
+
+%% Add file path
+sessionPath = '/data/projects/Chunshu/NCTUStandardLevel2/session/';
+
+%% load raw EEG data
+channelToRemove = {'vehicle position'};
+bandPassFilt = [0.5 100];
+resamplingRate = 250;
+sessNum = 1:80;
+thresASR = [1, 2.5, 3, 5, 10, 20, 30, 40, 50, 70, 100, 200, 500, 1000];
+
+for i = 1:length(sessNum)
+    rawdataPath = dir([sessionPath,sprintf('%d/',sessNum(i)),'*.set']);
+    rawdataPath = [rawdataPath.folder filesep rawdataPath.name];
+    EEG_raw = pop_loadset(rawdataPath);
+    chanLabels = {EEG_raw.chanlocs.labels};
+    
+    % Remove vehicle channel and reconstructed channel
+    if ~isempty(EEG_raw.etc.noiseDetection.reference.interpolatedChannels)
+        reconstChannel = chanLabels(EEG_raw.etc.noiseDetection.reference.interpolatedChannels);
+        channelToRemove = [{'vehicle position'},reconstChannel];
+    end
+    EEG_raw = pop_select(EEG_raw,'nochannel',channelToRemove);
+    
+    % Band pass and resample
+    EEG = pop_eegfiltnew(EEG, bandPassFilt(1), bandPassFilt(2), [], 0, [], 0);
+    EEG = pop_resample( EEG, resamplingRate);
+    
+    
+    
+    
+    
+end
